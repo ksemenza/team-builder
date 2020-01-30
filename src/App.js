509dd2a1f24964f6d947c4926f1efe456694c72d
unknown <ksemenza13@gmail.com>
2020-01-30 10:20:19 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Members from './components/Members';
+import TeamMemberForm from './components/TeamMemberForm';
+
 
 function App() {
+  const [ teamList, setTeamList ] = useState([
+    {
+      id: '0',
+      name: 'Jojo Zhang',
+      email: 'jojo-zhang@gmail.com',
+      role: 'Project Lead',
+
+    },
+    { id: '1',
+      name: 'Raj Upadhyaya',
+      email: 'xrajupadhyayax@gmail.com',
+      role: 'Front-End Engineer',
+
+    },
+    {
+      id: '2',
+      name: 'Michael Nunes',
+      email: 'miketnunes@gmail.com',
+      role: 'Back-End Engineer', 
+    },
+    {
+      id: '3',
+      name: 'Kim Semenza',
+      email: 'ksemenza.247@gmail.com',
+      role: 'UI Developer',
+    }
+  ]);
+
+  const addNewMember = member => {
+    const newMember = {
+      id: Date.now(),
+      name: member.name,
+      email: member.email,
+      role: member.role,
+
+    };
+    setTeamList([ ...teamList, newMember]);
+  }
+
+  const [memberToEdit, setMemberToEdit] = useState('');
+
+  const addMemberToEdit = member => {
+    setMemberToEdit(member);
+  }
+
+  const editMember = member => {
+    let restOfList = teamList.filter(person => person.id !== member.id);
+    restOfList.unshift(member);
+    setTeamList(restOfList);
+    setMemberToEdit('');
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading">
+        <h1>Development Team</h1>
+      </div>
+     
+      <TeamMemberForm addNewMember={addNewMember} memberToEdit={memberToEdit}
+        editMember={editMember}/>
+      <Members teamMembers={teamList} addMemberToEdit={addMemberToEdit}/>
     </div>
   );
 }
---
+++ src/components/TeamMemberForm.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+
+const TeamMemberForm = props => {
+    const [person, setPerson] = useState({name: '', email: '', role: ''});
+
+    const handleChanges = event => {
+        setPerson( { ...person, [event.target.name]: event.target.value});
+    }
+
+    const submitForm = event => {
+        event.preventDefault();
+        if (props.memberToEdit) {
+            props.editMember(person);
+        } else {
+        props.addNewMember(person);
+        }
+        setPerson({name: '', email: '', role: ''});
+    }
+
+    useEffect(() => {
+        console.log(props.memberToEdit);
+        setPerson(props.memberToEdit);
+    }, [props.memberToEdit]);
+
+    return (
+        <form onSubmit={submitForm}>
+                <label htmlFor='inputName'>Full Name</label>
+                <input id='inputName'
+                    type='text'
+                    name='name'
+                    placeholder="Full Name"
+                    onChange={handleChanges}
+                    value={person.name}
+                />
+               <label htmlFor='inputEmail'>Email </label>
+                <input id='inputEmail'
+                    type='email'
+                    name='email'
+                    placeholder="Email"
+                    onChange={handleChanges}
+                    value={person.email}
+                />
+                <label htmlFor='inputJobRole'> Role </label>
+                <select type='select' id='inputJobRole' 
+                    onChange={handleChanges} 
+                    name='role' 
+                    value={person.role}>
+                    <option value='Project Manager'>Project Lead</option>
+                    <option value='UX Designer'>UX Designer</option>
+                    <option value='Front-End Engineer'>Front-End Engineer</option>
+                    <option value='Back-End Engineer'>Back-End Engineer</option>
+                    <option value='UI Developer'>UI Developer</option>
+                    <option value='Mobile Developer'>Mobile Developer</option>
+                    <option value='Data Scientist'>Data Scientist</option>
+                    <option value='Other Position'>Other Position</option>
+                </select>
+     
+          
+            <button type='submit'>{
+                props.memberToEdit !== ''? 'Submit': 'Add'}</button>
+        </form>
+    )
+}
+
+export default TeamMemberForm;
